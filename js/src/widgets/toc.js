ed14fb56a4db61980d62fb4c8ee48d20c221de69
John Howard <john.brooks.howard@gmail.com>
2018-10-28 06:12:36 +0000

    Update toc.js: plain language instead of 'noIndex'
    Replaced code-talk 'noIndex' with plain language 'No index for this item'

--- js/src/widgets/toc.js
+++ js/src/widgets/toc.js
@@ -375,7 +375,7 @@
     emptyTemplate: $.Handlebars.compile([
       '<ul class="toc">',
       '<li class="leaf-item open">',
-      '<h2><span>{{t "noIndex"}}</span></h2>',
+      '<h2><span>{{t "No index for this item"}}</span></h2>',
       '</ul>',
     ].join('')),
 
